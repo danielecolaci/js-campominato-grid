402d948da472a5da42fbf325aa4295355b7fb531
danielecolaci <danielecolaci@outlook.com>
2024-02-28 16:00:59 +0100

    Add html element and take button element in js to create grid element

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
-console.log('ok');
+//Prendo il button 'start'
+let buttonElement = document.querySelector('#start');
+console.log(buttonElement);
+
+buttonElement.addEventListener('click', function () {
+
+    const gridElement = document.querySelector('#grid');
+    console.log(gridElement);
+})
