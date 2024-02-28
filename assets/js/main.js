8db3fc62d3817887476e9524abecd0676a009a2f
danielecolaci <danielecolaci@outlook.com>
2024-02-28 16:29:49 +0100

    Create in console box element on click

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,4 +6,17 @@ buttonElement.addEventListener('click', function () {
 
     const gridElement = document.querySelector('#grid');
     console.log(gridElement);
+
+    let userLevel = 100;
+
+    for (let i = 0; i < userLevel; i++) {
+
+        const myElement = document.createElement('div');
+        myElement.classList.add('box');
+        myElement.innerHTML = i + 1;
+        console.log(myElement);
+
+    }
+
+
 })
